d042573fa1ea64f9f7f45300ce268d76f81aebe0
fxt0706 <fxt0706@gmail.com>
2020-03-18 18:04:14 +0800

    update

--- lib/data/common.dart
+++ lib/data/common.dart
@@ -1,3 +1,31 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+SharedPreferences prefs;
+
 String globalAuthorization;
 String globalUserName;
 String globalUserEmail;
+
+List<String> keywords = ['auth','id', 'name', 'email', 'qqcheck', 'emailcheck', 'avatar',];
+
+
+initData() async{
+  prefs = await SharedPreferences.getInstance();
+  try {
+    prefs.getBool('ifInit');
+  } catch (e) {
+    for(var item in keywords) {
+      item.contains('check')
+      ? prefs.setBool(item, false)
+      : prefs.setString(item, '');
+    }
+  }
+}
+
+checkData(String label) {
+  try {
+    prefs.getString(label);
+  } catch (e) {
+    prefs.setString(label, '');
+  }
+}
